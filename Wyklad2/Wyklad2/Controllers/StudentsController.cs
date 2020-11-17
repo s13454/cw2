de3cf6e125f666896895afc2182a06691006bd24
adaosowiecka <40485375+adaosowiecka@users.noreply.github.com>
2020-11-17 17:18:36 +0100

    test czy dziala
    fdsfds

---
+++ Wyklad2/Wyklad2/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wyklad2.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet("cosInnego")]
+        public string GetStudents() 
+        {
+            return "Jan, Andrzej, Marcin";
+        }
+    }
+}
---
+++ Wyklad2/Wyklad2/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wyklad2.Models
+{
+    public class Student { 
+        public int IdStudent { get; set;}
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+
+    }
+}
